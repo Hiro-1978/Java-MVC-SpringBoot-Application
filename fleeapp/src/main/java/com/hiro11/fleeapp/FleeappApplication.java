1c41001ec5b680f97c583daedd1946c468aaf417
Hiro-1978 <78284161+Hiro-1978@users.noreply.github.com>
2021-11-03 11:36:00 +0700

    Initial commit

---
+++ fleeapp/src/main/java/com/hiro11/fleeapp/FleeappApplication.java
@@ -0,0 +1,13 @@
+package com.hiro11.fleeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleeappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FleeappApplication.class, args);
+	}
+
+}
---
+++ fleeapp/src/test/java/com/hiro11/fleeapp/FleeappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hiro11.fleeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FleeappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
