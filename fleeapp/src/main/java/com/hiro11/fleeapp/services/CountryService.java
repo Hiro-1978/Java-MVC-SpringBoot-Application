ee3b3a019dde194815db7cc9779b3034eb2c285a
Hiro-1978 <78284161+Hiro-1978@users.noreply.github.com>
2021-11-12 11:48:33 +0700

    fleeapp
    Display the Edit Modal Using JavaScript

--- fleeapp/src/main/java/com/hiro11/fleeapp/services/CountryService.java
+++ fleeapp/src/main/java/com/hiro11/fleeapp/services/CountryService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CountryService {
@@ -15,11 +16,26 @@ public class CountryService {
 
     //Return list of countries
     public List<Country> getCountries() {
+
         return countryRepository.findAll();
     }
 
-    //Save new country
+    //Save new country.js
     public void save(Country country){
+
         countryRepository.save(country);
     }
+
+    //Get Country By Id
+    public Optional<Country> findById(int id) {
+
+        return countryRepository.findById(id);
+    }
+
+    //Delete Country
+    public void delete(int id) {
+
+        countryRepository.deleteById(id);
+    }
+
 }
