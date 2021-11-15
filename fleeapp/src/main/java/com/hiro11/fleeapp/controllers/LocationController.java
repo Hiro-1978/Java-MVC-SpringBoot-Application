952b0a7a35d90ccb6a50cec08d22658707fee461
Hiro-1978 <78284161+Hiro-1978@users.noreply.github.com>
2021-11-15 19:15:33 +0700

    fleeapp
    return html

--- fleeapp/src/main/java/com/hiro11/fleeapp/controllers/LocationController.java
+++ fleeapp/src/main/java/com/hiro11/fleeapp/controllers/LocationController.java
@@ -1,8 +1,62 @@
 package com.hiro11.fleeapp.controllers;
 
+import com.hiro11.fleeapp.models.Location;
+import com.hiro11.fleeapp.services.CountryService;
+import com.hiro11.fleeapp.services.LocationService;
+import com.hiro11.fleeapp.services.StateService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class LocationController {
 
+    @Autowired
+    private LocationService locationService;
+    @Autowired
+    private CountryService countryService;
+    @Autowired
+    private StateService stateService;
+
+    @GetMapping("locations")
+    public String findAll(Model model){
+        model.addAttribute("locations", locationService.findAll());
+        model.addAttribute("countries", countryService.findAll());
+        model.addAttribute("states", stateService.findAll());
+
+        return "location";
+    }
+
+    @GetMapping("locations/findById")
+    @ResponseBody
+    public Optional<Location> findById(Integer id) {
+        return locationService.findById(id);
+    }
+
+    @GetMapping("locations/findByDescriptionContaining/{description}")
+    public List<Location> findByDescriptionContaining(@PathVariable("description") String description) {
+        return locationService.findByDescriptionContaining(description);
+    }
+
+    @PostMapping("locations/addNew")
+    public String save(Location location) {
+        locationService.save(location);
+        return "redirect:/locations";
+    }
+
+    @RequestMapping(value="locations/update", method = {RequestMethod.PUT, RequestMethod.GET})
+    public String update(Location location) {
+        locationService.save(location);
+        return "redirect:/locations";
+    }
+
+    @RequestMapping(value="locations/delete", method = {RequestMethod.DELETE, RequestMethod.GET})
+    public String deleteById(Integer id) {
+        locationService.deleteById(id);
+        return "redirect:/locations";
+    }
 }
--- fleeapp/src/main/java/com/hiro11/fleeapp/controllers/StateController.java
+++ fleeapp/src/main/java/com/hiro11/fleeapp/controllers/StateController.java
@@ -13,40 +13,42 @@ import java.util.Optional;
 @Controller
 public class StateController {
 
-        @Autowired
-        private StateService stateService;
-        @Autowired private CountryService countryService;
 
-        //Get All States
-        @GetMapping("states")
-        public String findAll(Model model){
-            model.addAttribute("states", stateService.getState());
-            model.addAttribute("countries", countryService.getCountries());
-            return "state";
-        }
+    @Autowired
+    private StateService stateService;
+    @Autowired private CountryService countryService;
 
-        @RequestMapping("states/findById")
-        @ResponseBody
-        public Optional<State> findById(int id){
-            return stateService.findById(id);
-        }
+    //Get All States
+    @GetMapping("states")
+    public String findAll(Model model){
+        model.addAttribute("states", stateService.getState());
+        model.addAttribute("countries", countryService.getCountries());
+        return "state";
+    }
 
-        //Add State
-        @PostMapping(value="states/addNew")
-        public String addNew(State state) {
-            stateService.save(state);
-            return "redirect:/states";
-        }
+    @RequestMapping("states/findById")
+    @ResponseBody
+    public Optional<State> findById(int id){
+        return stateService.findById(id);
+    }
 
-        @RequestMapping(value="states/update", method = {RequestMethod.PUT, RequestMethod.GET})
-        public String update(State state) {
-            stateService.save(state);
-            return "redirect:/states";
-        }
+    //Add State
+    @PostMapping(value="states/addNew")
+    public String addNew(State state) {
+        stateService.save(state);
+        return "redirect:/states";
+    }
 
-        @RequestMapping(value="states/delete", method = {RequestMethod.DELETE, RequestMethod.GET})
-        public String delete(int id) {
-            stateService.delete(id);
-            return "redirect:/states";
-        }
+    @RequestMapping(value="states/update", method = {RequestMethod.PUT, RequestMethod.GET})
+    public String update(State state) {
+        stateService.save(state);
+        return "redirect:/states";
+    }
+
+    @RequestMapping(value="states/delete", method = {RequestMethod.DELETE, RequestMethod.GET})
+    public String delete(int id) {
+        stateService.delete(id);
+        return "redirect:/states";
+    }
 }
+
