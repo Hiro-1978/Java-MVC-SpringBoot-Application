16fb0b8bd59b976b407e332c57b431f55ff7fe1f
Hiro-1978 <78284161+Hiro-1978@users.noreply.github.com>
2021-11-17 17:58:37 +0700

    fleeapp
    Select, Insert, Update and Delete Vehicle Movements

--- fleeapp/src/main/java/com/hiro11/fleeapp/controllers/VehicleMovementController.java
+++ fleeapp/src/main/java/com/hiro11/fleeapp/controllers/VehicleMovementController.java
@@ -1,8 +1,58 @@
 package com.hiro11.fleeapp.controllers;
 
+import com.hiro11.fleeapp.models.VehicleMovement;
+import com.hiro11.fleeapp.services.LocationService;
+import com.hiro11.fleeapp.services.VehicleMovementService;
+import com.hiro11.fleeapp.services.VehicleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Controller
 public class VehicleMovementController {
 
+    @Autowired
+    private VehicleMovementService vehicleMovementService;
+    @Autowired
+    private LocationService locationService;
+    @Autowired
+    private VehicleService vehicleService;
+
+    //Get All VehicleMovements
+    @GetMapping("vehicleMovements")
+    public String findAll(Model model) {
+        model.addAttribute("vehicleMovements", vehicleMovementService.getVehicleMovements());
+        model.addAttribute("locations", locationService.getLocations());
+        model.addAttribute("vehicles", vehicleService.getVehicles());
+
+        return "vehicleMovement";
+    }
+
+    @RequestMapping("vehicleMovements/findById")
+    @ResponseBody
+    public Optional<VehicleMovement> findById(Integer id) {
+        return vehicleMovementService.findById(id);
+    }
+
+    //Add VehicleMovement
+    @PostMapping(value = "vehicleMovements/addNew")
+    public String addNew(VehicleMovement vehicleMovement) {
+        vehicleMovementService.save(vehicleMovement);
+        return "redirect:/vehicleMovements";
+    }
+
+    @RequestMapping(value = "vehicleMovements/update", method = {RequestMethod.PUT, RequestMethod.GET})
+    public String update(VehicleMovement vehicleMovement) {
+        vehicleMovementService.save(vehicleMovement);
+        return "redirect:/vehicleMovements";
+    }
+
+    @RequestMapping(value = "vehicleMovements/delete", method = {RequestMethod.DELETE, RequestMethod.GET})
+    public String delete(Integer id) {
+        vehicleMovementService.delete(id);
+        return "redirect:/vehicleMovements";
+    }
 }
--- fleeapp/src/main/java/com/hiro11/fleeapp/services/VehicleMovementService.java
+++ fleeapp/src/main/java/com/hiro11/fleeapp/services/VehicleMovementService.java
@@ -1,8 +1,40 @@
 package com.hiro11.fleeapp.services;
 
+import com.hiro11.fleeapp.models.VehicleMovement;
+import com.hiro11.fleeapp.repositories.VehicleMovementRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class VehicleMovementService {
 
+    @Autowired
+    private VehicleMovementRepository vehicleMovementRepository;
+
+    //Get All VehicleMovements
+    public List<VehicleMovement> getVehicleMovements(){
+        return vehicleMovementRepository.findAll();
+    }
+
+    //Get VehicleMovement By Id
+    public Optional<VehicleMovement> findById(int id) {
+        return vehicleMovementRepository.findById(id);
+    }
+
+    //Delete VehicleMovement
+    public void delete(int id) {
+        vehicleMovementRepository.deleteById(id);
+    }
+
+    //Update VehicleMovement
+    public void save(VehicleMovement vehicleMovement) {
+        vehicleMovementRepository.save(vehicleMovement);
+    }
+
 }
+
