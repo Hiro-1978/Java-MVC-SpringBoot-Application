1527cdb86ee3d1a77a8c35d0343eecc5489a6c05
Hiro-1978 <78284161+Hiro-1978@users.noreply.github.com>
2021-11-17 18:07:36 +0700

    fleeapp
    Select, Insert, Update and Delete Vehicle Hires

--- fleeapp/src/main/java/com/hiro11/fleeapp/services/VehicleHireService.java
+++ fleeapp/src/main/java/com/hiro11/fleeapp/services/VehicleHireService.java
@@ -1,8 +1,36 @@
 package com.hiro11.fleeapp.services;
 
+import com.hiro11.fleeapp.models.VehicleHire;
+import com.hiro11.fleeapp.repositories.VehicleHireRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class VehicleHireService {
 
+    @Autowired
+    private VehicleHireRepository vehicleHireRepository;
+
+    //Get All VehicleHires
+    public List<VehicleHire> getVehicleHires(){
+        return vehicleHireRepository.findAll();
+    }
+
+    //Get VehicleHire By Id
+    public Optional<VehicleHire> findById(int id) {
+        return vehicleHireRepository.findById(id);
+    }
+
+    //Delete VehicleHire
+    public void delete(int id) {
+        vehicleHireRepository.deleteById(id);
+    }
+
+    //Update VehicleHire
+    public void save(VehicleHire vehicleHire) {
+        vehicleHireRepository.save(vehicleHire);
+    }
 }
