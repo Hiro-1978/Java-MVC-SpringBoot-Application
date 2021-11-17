53285d49964b6f57f72f6669fd96edd086d3186d
Hiro-1978 <78284161+Hiro-1978@users.noreply.github.com>
2021-11-17 13:10:31 +0700

    fleeapp
    Select, Insert, Update and Delete Client Data

--- fleeapp/src/main/java/com/hiro11/fleeapp/controllers/ClientController.java
+++ fleeapp/src/main/java/com/hiro11/fleeapp/controllers/ClientController.java
@@ -1,8 +1,57 @@
 package com.hiro11.fleeapp.controllers;
 
+import com.hiro11.fleeapp.models.Client;
+import com.hiro11.fleeapp.services.ClientService;
+import com.hiro11.fleeapp.services.CountryService;
+import com.hiro11.fleeapp.services.StateService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Controller
 public class ClientController {
+    @Autowired
+    private StateService stateService;
+    @Autowired
+    private CountryService countryService;
+    @Autowired
+    private ClientService clientService;
 
+    //Get All Clients
+    @GetMapping("/clients")
+    public String findAll(Model model){
+        model.addAttribute("countries", countryService.getCountries());
+        model.addAttribute("states", stateService.getState());
+        model.addAttribute("clients", clientService.getClients());
+        return "client";
+    }
+
+    @RequestMapping("clients/findById")
+    @ResponseBody
+    public Optional<Client> findById(Integer id)
+    {
+        return clientService.findById(id);
+    }
+
+    //Add Client
+    @PostMapping(value="clients/addNew")
+    public String addNew(Client client) {
+        clientService.save(client);
+        return "redirect:/clients";
+    }
+
+    @RequestMapping(value="clients/update", method = {RequestMethod.PUT, RequestMethod.GET})
+    public String update(Client client) {
+        clientService.save(client);
+        return "redirect:/clients";
+    }
+
+    @RequestMapping(value="clients/delete", method = {RequestMethod.DELETE, RequestMethod.GET})
+    public String delete(Integer id) {
+        clientService.delete(id);
+        return "redirect:/clients";
+    }
 }
--- fleeapp/src/main/java/com/hiro11/fleeapp/services/ClientService.java
+++ fleeapp/src/main/java/com/hiro11/fleeapp/services/ClientService.java
@@ -1,8 +1,35 @@
 package com.hiro11.fleeapp.services;
 
+import com.hiro11.fleeapp.models.Client;
+import com.hiro11.fleeapp.repositories.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ClientService {
+    @Autowired
+    private ClientRepository clientRepository;
 
+    //Get All Clients
+    public List<Client> getClients(){
+        return clientRepository.findAll();
+    }
+
+    //Get Client By Id
+    public Optional<Client> findById(int id) {
+        return clientRepository.findById(id);
+    }
+
+    //Delete Client
+    public void delete(int id) {
+        clientRepository.deleteById(id);
+    }
+
+    //Update Client
+    public void save(Client client) {
+        clientRepository.save(client);
+    }
 }
